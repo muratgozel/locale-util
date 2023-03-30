305b8df1fd7c32c6db946a34fa99298fd7571a89
Murat Gözel <murat@gozel.com.tr>
2023-03-30 17:28:26 +0300

    improved type support for different environments

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,15 +1,15 @@
 declare module 'locale-util' {
-    export type CountryCode = import('#src/data/countryCodes').CountryCode
-    export type LanguageCode = import('#src/data/languageCodes').LanguageCode
-    export type LanguageCodeExtended = import('#src/data/languageCodesExtended').LanguageCodeExtended
-    export type CurrencyCode = import('#src/data/currencyCodes').CurrencyCode
-    export type CountryLanguages = import('#src/data/countryLanguages').CountryLanguages
-    export type CountryLanguagesExtended = import('#src/data/countryLanguagesExtended').CountryLanguagesExtended
-    export type Language = import('#src/data/languages').Language
-    export type Currency = import('#src/data/currencies').Currency
-    export type Country = import('#src/data/countries').Country
-    export type Timezone = import('#src/data/timezones').Timezone
-    export type CountryCallingCodes = import('#src/data/countryCallingCodes').CountryCallingCodes
+    export type CountryCode = import('../src/data/countryCodes').CountryCode
+    export type LanguageCode = import('../src/data/languageCodes').LanguageCode
+    export type LanguageCodeExtended = import('../src/data/languageCodesExtended').LanguageCodeExtended
+    export type CurrencyCode = import('../src/data/currencyCodes').CurrencyCode
+    export type CountryLanguages = import('../src/data/countryLanguages').CountryLanguages
+    export type CountryLanguagesExtended = import('../src/data/countryLanguagesExtended').CountryLanguagesExtended
+    export type Language = import('../src/data/languages').Language
+    export type Currency = import('../src/data/currencies').Currency
+    export type Country = import('../src/data/countries').Country
+    export type Timezone = import('../src/data/timezones').Timezone
+    export type CountryCallingCodes = import('../src/data/countryCallingCodes').CountryCallingCodes
 
     export type isCountryCode = (v: unknown) => v is CountryCode
     export type findCountry = (v: CountryCode) => Country | undefined
@@ -22,15 +22,13 @@ declare module 'locale-util' {
     export type findCountryTimezones = (v: CountryCode) => Timezone[] | undefined
     export type findTimezoneOffset = (v: Timezone) => number | undefined
 
-    export {countryCodes} from '#src/data/countryCodes'
-    export {currencyCodes} from '#src/data/currencyCodes'
-    export {countryLanguages} from '#src/data/countryLanguages'
-    export {languageCodes} from '#src/data/languageCodes'
-    export {countries} from '#src/data/countries'
-    export {currencies} from '#src/data/currencies'
-    export {languages} from '#src/data/languages'
-    export {countryCallingCodes} from '#src/data/countryCallingCodes'
-    export {timezones} from '#src/data/timezones'
-
-    export as namespace localeUtil
+    export const countryCodes: CountryCode[]
+    export const currencyCodes: CurrencyCode[]
+    export const countryLanguages: CountryLanguages
+    export const languageCodes: LanguageCode[]
+    export const countries: Country[]
+    export const currencies: Currency[]
+    export const languages: Language[]
+    export const countryCallingCodes: CountryCallingCodes
+    export const timezones: Timezone[]
 }
